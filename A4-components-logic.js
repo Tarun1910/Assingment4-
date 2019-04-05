fb34ea61d622dad0e5d4353e5c81bc3f553fd466
Tarun Kumar <tarun1910.kumar@gmail.com>
2019-04-04 21:40:54 -0400

    Tarun Kumar n01333782 secONB Assingment 4 files

---
+++ A4-components-logic.js
@@ -0,0 +1,19 @@
+﻿//ASSIGNMENT 4 WEB COMPONENTS
+//LOGIC FILE
+//IN THIS FILE YOU WILL SIMPLY GRAB YOUR CUSTOM ELEMENT AND ATTACH YOUR METHOD FROM YOUR MODULE TO IT.
+window.onload = init;
+function init()
+{
+// ========take example from in class lab 8
+	var customTemplate = document.querySelector("#humber_template");
+	var humberProto = Object.create(HTMLElement.prototype);
+	humberProto.createdCallback = function(){
+    var root = this.attachShadow({mode:'open'});
+    root.appendChild(document.importNode(customTemplate.content,true));
+	};
+	var NameBadge = document.registerElement('humber-clock',{prototype:humberProto});
+	
+    //calling object from module.js myModNS.citeMeFn
+   document.getElementById("showDate").innerHTML= myModNS.citeMeFn;
+
+} 
---
+++ A4-components-module.js
@@ -0,0 +1,17 @@
+﻿//ASSIGNMENT 4 WEB COMPONENTS
+//MODULE FILE
+//IN THIS FILE YOU WILL CREATE YOUR CUSTOM MODULE FOR YOUR REQUIRED FUNCTIONALITY AND EXPOSE IT THROUGH A 'PUBLICLY' ACCESSIBLE METHOD.
+
+
+//taken example from semester 1 math class
+	var myModNS = (function(){
+    var myDate1 = new Date(); 
+	var lastDate1 = new Date("April 26,2019 17:00:00");
+	var dueDate1 = lastDate1-myDate1;
+	var remDate = dueDate1/86400000;
+	var remDate1 =Math.round(remDate);
+	var module = {
+			citeMeFn : remDate1+" day(s) left in the semester"
+    };
+	return module;
+})();
